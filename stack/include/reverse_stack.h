1bb2fb2012b387fff9c7c85eeb368646b7171d9b
caviar95 <yzj1995@126.com>
2024-11-09 17:19:04 +0800

    comment for reverse_stack

--- stack/include/reverse_stack.h
+++ stack/include/reverse_stack.h
@@ -2,7 +2,7 @@
  * @Author: Caviar
  * @Date: 2024-11-09 16:44:45
  * @LastEditors: Caviar
- * @LastEditTime: 2024-11-09 17:08:13
+ * @LastEditTime: 2024-11-09 17:17:44
  * @Description: 
  */
 
@@ -12,6 +12,8 @@
 
 using std::stack;
 
+// step1: 将stack的栈底元素返回并移除
 int GetAndRemoveLastElement(stack<int> &sta);
 
+// step2: 逆序stack
 void GetAndRemoveLastElement_Version2(stack<int> &sta);
